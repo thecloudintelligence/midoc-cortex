437a21db289954c3220eb1e61439336837900339
tanish-tci <tanishk@thecloudintelligence.com>
2026-06-02 12:51:37 +0530

    update: dist to build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
